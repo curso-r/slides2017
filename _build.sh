7243840119133060864aeea4d90255a550d1b9d0
Fernando Corrêa <fptcorrea@gmail.com>
2017-02-02 16:58:31 -0200

    adiciona nova estrutura ao pacote slides

---
+++ _build.sh
@@ -0,0 +1,1 @@
+Rscript -e build.R
---
+++ build.R
@@ -0,0 +1,20 @@
+library(knitr)
+library(magrittr)
+
+ex_dir <- normalizePath("content/")
+
+dir.create(ex_dir)
+
+rmds <- list.files(pattern = "\\.Rmd", recursive = T, full.names = T)
+
+htmls <- gsub("\\.Rmd","\\.html",rmds) %>% 
+  basename %>% 
+  paste0(ex_dir,"/", .)
+
+for (arq in list.files(pattern = "\\.Rmd$", recursive = T, full.names = T)){
+#' Estou usando um for porque gerar o markdown é um side effect
+  rmarkdown::render(arq, intermediates_dir = ex_dir) 
+}
+
+list.files(pattern = "\\.html$", recursive = T, full.names = T) %>% 
+  file.copy(htmls)
