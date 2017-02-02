0e3f6cbf0192d95d724444b0db3f49b157ec0b20
Fernando Corrêa <fptcorrea@gmail.com>
2017-02-02 17:04:29 -0200

    remove conteúdo inútil complementa o setup do travis

---
+++ _deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "fptcorrea@gmail.com"
+git config --global user.name "azeloc"
+
+git clone -b content https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git out
+cd out
+cp -r ../content/* ./
+git add --all *
+git commit -m "Update the PU" || true
+git push -q origin content
