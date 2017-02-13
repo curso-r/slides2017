0b06ffbe88d807133f14e467cf0fd715a62bc584
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-13 13:48:46 -0200

    simplificando exemplo

--- aula7/exemplo5.R
+++ aula7/exemplo5.R
@@ -1,53 +1,45 @@
 library(purrr)
-# validação cruzada -------------------------------------------------------
 
-cross_val_logistica  <- function(formula, dataset, prop = 0.75, repet = 1){
-  
-  dataset_sem_na <- dataset %>% 
-    filter(complete.cases(.))
-  
-  ajusta_1_logistica <- function(){
-    
-    indices <- sample(1:nrow(dataset), nrow(dataset)*prop)
-    
-    treino <- dataset_sem_na[indices,]
-    teste <- dataset_sem_na[-indices,]
-  
+# validação cruzada -------------------------------------------------------
+dataset_sem_na <- titanic %>% 
+  filter(complete.cases(.)) %>% 
+  mutate_if(is.character, funs(as.factor(.)))
+
+cross_val_logistica  <- function(formula, dataset, prop = 0.75, repet = 1) {
+  ajustar_logistica <- function() {
+    indices <- sample(1:nrow(dataset), nrow(dataset) * prop)
+    treino <- dataset[indices,]
+    teste <- dataset[-indices,]
     modelo <- glm(formula, data = treino, family = 'binomial')
     predicao <- predict(modelo, newdata = teste, type = 'response') %>% 
-      round %>% 
-      factor(levels = c(0,1))
-
-    erro <- sum(predicao==teste$Survived, na.rm = T)/nrow(teste)
+      round() %>% 
+      factor(levels = c(0, 1))
     
+    erro <- sum(predicao == teste$Survived, na.rm = TRUE) / nrow(teste)
     return(erro)
   }
   
-  erro <- map_dbl(1:repet, ~ajusta_1_logistica())
-  
+  erro <- map_dbl(1:repet, ~ajustar_logistica())
   list(modelo = formula, erro_cv = mean(erro))
 }
 
-cross_val_logistica(Survived ~ Sex + Age + Fare, dataset = titanic, prop = 0.75, repet = 10)
+cross_val_logistica(Survived ~ Sex + Age + Fare, 
+                    dataset = dataset_sem_na, 
+                    prop = 0.75, 
+                    repet = 10)
 
-formulas <- map2(list(names(titanic)), #primeiro argumento
-     c(1:5), #segundo argumento
-     gera_formulas, #função que será aplicada
-     response_var = "Survived", #parâmetros adicionais
-     exclude_vars = c("Name","Cabin","Ticket","PassengerId","Embarked") # parâmetros adicionais
-) %>% 
-  unlist()
+# carregar formulas no exemplo2.R
 
-titanic <- titanic %>% 
-  mutate_if(is.character, funs(as.factor(.)))
-
-titanic$Age <- as.numeric(titanic$Age)
-titanic$Fare <- as.numeric(titanic$Fare)
-titanic$Parch <- as.numeric(titanic$Parch)
-titanic$SibSp <- as.numeric(titanic$SibSp)
-
-ajustes <- map(formulas, cross_val_logistica, dataset = titanic, prop = 0.75, repet = 10)
+ajustes <- map(formulas, 
+               cross_val_logistica, 
+               dataset = dataset_sem_na, 
+               prop = 0.75, 
+               repet = 10)
 
 modelos_ordenados <- ajustes %>%
   sort_by(~-.x$erro_cv) %>% 
   map_chr(~.x$modelo)
+
+modelos_ordenados
+
+
