61048fb63399cdead0f27156fe3b69ea9209eae1
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-13 13:48:35 -0200

    identacao

--- aula7/exemplo4.R
+++ aula7/exemplo4.R
@@ -2,12 +2,16 @@ library(purrr)
 
 # predicados --------------------------------------------------------------
 
-exemplo <- list(a = list(aplica = T, valor = 11),
-                b = list(aplica = F, valor = 10),
-                c = list(aplica = F, valor = 3),
-                d = list(aplica = T, valor = 4))
+exemplo <- list(a = list(aplica = TRUE, valor = 11),
+                b = list(aplica = FALSE, valor = 10),
+                c = list(aplica = FALSE, valor = 3),
+                d = list(aplica = TRUE, valor = 4))
 
-keep(exemplo, ~ .x$aplica)
-discard(exemplo, ~ .x$aplica)
+# retorna um subset dos verbos
+
+keep(exemplo, ~.x$aplica)
+discard(exemplo, ~.x$aplica)
+
+# retorna TRUE ou FALSE
 every(exemplo, ~ .x$aplica)
 some(exemplo, ~ .x$aplica)
