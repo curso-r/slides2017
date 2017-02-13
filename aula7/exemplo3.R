de1cbd796d6072f04974b345ae938ab54ad06e86
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-13 13:48:28 -0200

    purrr::walk

--- aula7/exemplo3.R
+++ aula7/exemplo3.R
@@ -1,30 +1,29 @@
 library(ggplot2)
+library(dplyr)
 
 # transformação -----------------------------------------------------------
 
-plota_mpg_wt <- function(x){
+plotar_mpg_wt <- function(x) {
   x %>% 
     ggplot(aes(x = wt, y = mpg)) +
     geom_point() + 
     theme_bw()
 }
-  
-  salva_plot <- function(linha, nomes){
-    ggsave(plot =   linha$graficos[[1]], filename = linha$nome)
-    return("plotou")
-  }
-  
-  nomes_dos_arquivos <- paste0("aula7/ex3_out/",1:3,'.png')
-  
-  df_graficos <- mtcars %>% 
-    group_by(cyl) %>% 
-    by_slice(plota_mpg_wt, .to = "graficos") %>% 
-    mutate(nome = nomes_dos_arquivos)
-  
-  dir.create('aula7/ex3_out')
-  
-  df_graficos %>%
-    by_row(salva_plot, .to = 'resultado')
-  
-  
-  
+
+salvar_plot <- function(linha, nomes) {
+  ggsave(plot = linha$graficos[[1]], filename = linha$nome)
+}
+
+nomes_dos_arquivos <- paste0("aula7/ex3_out/", 1:3, '.png')
+
+df_graficos <- mtcars %>% 
+  group_by(cyl) %>% 
+  by_slice(plotar_mpg_wt, .to = "graficos") %>% 
+  mutate(nome = nomes_dos_arquivos)
+
+dir.create('aula7/ex3_out', showWarnings = FALSE)
+
+df_graficos %>%
+  by_row(salvar_plot, .to = 'resultado')
+
+# Exercício: fazer isso com purrr::walk
