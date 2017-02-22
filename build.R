4bd7130f045cce4bd1d9e6659511ec509274cb86
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-22 12:54:58 -0300

    clean builds

--- build.R
+++ build.R
@@ -13,8 +13,15 @@ htmls <- gsub("\\.Rmd","\\.html",rmds) %>%
   paste0(ex_dir,"/", .)
 
 for (arq in list.files(pattern = "\\.Rmd$", recursive = T, full.names = T)){
-#' Estou usando um for porque gerar o markdown é um side effect
-  rmarkdown::render(arq, intermediates_dir = ex_dir) 
+  #' Estou usando um for porque gerar o markdown é um side effect
+  rmarkdown::render(arq, intermediates_dir = ex_dir)
+  pkgs <- names(sessionInfo()$otherPkgs)
+  pkgs <- paste('package:', pkgs, sep = "")
+  if (pkgs[1] != 'package:') {
+    try({
+      invisible(lapply(pkgs, detach, character.only = TRUE, unload = TRUE))
+    })
+  }
 }
 
 list.files(pattern = "\\.html$", recursive = T, full.names = T) %>% 
